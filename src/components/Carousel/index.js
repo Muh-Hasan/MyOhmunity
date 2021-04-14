38b55c3546ae7a33ba00d378e64e69a473c0d62f
Muhammad Hasan <muh.hasan.git@gmail.com>
2021-04-15 00:56:24 +0500

    fixed color match

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -1,20 +1,20 @@
 import React from "react"
-import { useMediaQuery } from "react-responsive"
+import { useBreakpoint } from "gatsby-plugin-breakpoints"
 import Slider from "react-slick"
 import ImgOne from "../../assets/yogis1.png"
 import SliderBodyRight from "../SliderBody/SliderBodyRight"
 
 const Carousel = () => {
+  const isMobile = useBreakpoint()
   const settings = {
-    dots: true,
-    fade: true,
+    dots: isMobile.md ? false : true,
+    fade: isMobile.md ? false : true,
     infinite: true,
+    arrows: false,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
   }
-  const isMobile = useMediaQuery({ query: "(max-width: 768px)" })
-
   return (
     <div>
       <Slider {...settings}>
@@ -27,15 +27,33 @@ the best teachers"
             description="Take classes in-person at nearby studios, outdoors or from the comfort of your home through our live virtual sessions. "
           />
         </div>
-        {/* <div>
-          <SliderBodyRight bgColor="" title="" img={} description='' />
+        <div>
+          <SliderBodyRight
+            bgColor="rgb(111, 109, 136)"
+            title="Practice with 
+the best teachers"
+            img={ImgOne}
+            description="Take classes in-person at nearby studios, outdoors or from the comfort of your home through our live virtual sessions. "
+          />
         </div>
         <div>
-          <SliderBodyRight bgColor="" title="" img={} description='' />
+          <SliderBodyRight
+            bgColor="white"
+            title="Practice with 
+the best teachers"
+            img={ImgOne}
+            description="Take classes in-person at nearby studios, outdoors or from the comfort of your home through our live virtual sessions. "
+          />
         </div>
         <div>
-          <SliderBodyRight bgColor="" title="" img={} description='' />
-        </div> */}
+          <SliderBodyRight
+            bgColor="rgb(111, 109, 136)"
+            title="Practice with 
+the best teachers"
+            img={ImgOne}
+            description="Take classes in-person at nearby studios, outdoors or from the comfort of your home through our live virtual sessions. "
+          />
+        </div>
       </Slider>
     </div>
   )
--- src/components/SliderBody/SliderBodyRight.js
+++ src/components/SliderBody/SliderBodyRight.js
@@ -1,13 +1,11 @@
-import React from 'react'
-import { useBreakpoint } from 'gatsby-plugin-breakpoints';
+import React from "react"
+import { useBreakpoint } from "gatsby-plugin-breakpoints"
 
-
-const SliderBodyRight = ({ bgColor , title, description , img }) => {
+const SliderBodyRight = ({ bgColor, title, description, img }) => {
   const isMobile = useBreakpoint()
-   console.log(isMobile);
-    return (
-        <div>
-            <div
+  return (
+    <div>
+      <div
         style={{
           background: isMobile.md
             ? `linear-gradient(to top, ${bgColor} 60%, white 40%)`
@@ -18,15 +16,31 @@ const SliderBodyRight = ({ bgColor , title, description , img }) => {
         <div className="flex items-center justify-center md:flex-col-reverse  container">
           <div className="w-2/5 text-left md:text-center md:w-full">
             <div className="flex md:justify-center">
-              <div className="w-14 h-0.5 bg-white"></div>
+              <div
+                className={`w-14 h-0.5 ${
+                  bgColor === "rgb(111, 109, 136)" ? "bg-white" : "bg-purple"
+                }`}
+              ></div>
             </div>
             <div className=" my-8 ">
-              <h2 className="font-roboMono text-white	text-3xl leading-10 uppercase">
+              <h2
+                className={`font-roboMono ${
+                  bgColor === "rgb(111, 109, 136)"
+                    ? "text-white"
+                    : "text-purple"
+                } text-3xl leading-10 uppercase`}
+              >
                 {title}
               </h2>
             </div>
             <div>
-              <p className="font-roboto text-white text-lg w-4/5 md:w-full">
+              <p
+                className={`font-roboto ${
+                  bgColor === "rgb(111, 109, 136)"
+                    ? "text-white"
+                    : "text-purple"
+                } text-lg w-4/5 md:w-full`}
+              >
                 {description}
               </p>
             </div>
@@ -36,8 +50,8 @@ const SliderBodyRight = ({ bgColor , title, description , img }) => {
           </div>
         </div>
       </div>
-        </div>
-    )
+    </div>
+  )
 }
 
 export default SliderBodyRight
